faa8d760a98c279f94d907d6fd21460117e3e210
priyanka1565 <104352972+priyanka1565@users.noreply.github.com>
2023-12-28 22:57:55 +0530

    added

--- backend/models/userModel.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const RegisterSchema = new mongoose.Schema({
-    username: String,
-    password: String
-})
-const RegisterModel = mongoose.model("Login", RegisterSchema);
-module.exports = RegisterModel;
