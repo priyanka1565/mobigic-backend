778102ddabdf0f090342ae5e4f7df01512eba80e
priyanka1565 <104352972+priyanka1565@users.noreply.github.com>
2023-12-28 14:58:17 +0530

    added

--- backend/connection/database.js
+++ backend/connection/database.js
@@ -1,11 +1,7 @@
-
-
-
 const mongoose = require("mongoose");
 
- function connect() {
-  return mongoose.connect(
-    "mongodb+srv://priyankaingle250:priya1565@cluster0.zrm5cra.mongodb.net/ticket_booking"
-  );
+function connectDataBase() {
+    return mongoose.connect("mongodb+srv://priyankaingle250:priya1565@cluster0.zrm5cra.mongodb.net/ticket_booking")
+
 }
-module.exports = { connect };
+module.exports = { connectDataBase }
